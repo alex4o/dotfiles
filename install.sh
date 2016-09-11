86f59789e7b22b26b8b87ee604fba5697578a1b5
alex4o <mrabonin@gmail.com>
2016-09-12 00:27:32 +0300

    added install file while playing ok!

---
+++ install.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+ln -s $(pwd)/.zsh $HOME/.zsh
+ln -s $(pwd)/.zshrc $HOME/.zshrc
+
+ln -s $(pwd)/.tmux.conf $HOME/.tmux.conf
+ln -s $(pwd)/.tmux $HOME/.tmux
+
+ln -s $(pwd)/.config/nvim $HOME/.config/nvim
+
