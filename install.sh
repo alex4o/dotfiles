7a9292e7e82b272b46693237e6d252a0bc8b4a52
Aleks Bonin <aleks@bonin.tech>
2020-10-07 14:03:03 +0300

    Adds gitconfig.

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-#/bin/bash
+#/usr/bin/evn bash
+
+set -o errexit
+set -o nounset
+set -o xtrace
+set -o pipefail
 
 ln -sfT $(pwd)/.zsh $HOME/.zsh
 ln -sfT $(pwd)/.zshrc $HOME/.zshrc
@@ -7,6 +12,21 @@ ln -sfT $(pwd)/.tmux.conf $HOME/.tmux.conf
 ln -sfT $(pwd)/.tmux $HOME/.tmux
 
 ln -sfT $(pwd)/.config/nvim $HOME/.config/nvim
+
+
+if [ ! -f "${HOME}/.gitconfig-local" ]; then
+	read FULLNAME
+	read EMAIL
+	cat << EOF > ${HOME}/.gitconfig-local
+		[user]
+		name = ${FULLNAME}
+		email = ${EMAIL}
+EOF
+fi
+
+ln -sfT $(pwd)/.gitconfig $HOME/.gitconfig
+
+
 
 mkdir -p ~/.vim
 touch ~/.vim/local.vim
